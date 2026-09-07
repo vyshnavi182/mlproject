04224ef1845e2b67ec32a654a484d49683f6eee7
Vyshnavi <your_github_email@example.com>
2026-09-07 10:01:03 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -42,3 +42,4 @@ if __name__ == "__main__":
     except Exception as e:
         logging.info("Divide by zero error")
         raise CustomException(e, sys)
+    
